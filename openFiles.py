8ac7393fe423765776db38735a7b59b50cf1bd53
Ayelet Bitton <bitton@slac.stanford.edu>
2010-07-06 15:42:56 -0700

    End of July 6: still adding doi reg expressions
    Code is nearly complete. Still waiting on adding
    final regular expressions to dictionary to use against
    outgoing clickes. Still some basic testing infrastructure
    left in the code. openFiles3.py is an older version of the
    current file, openFiles.py

--- openFiles.py
+++ openFiles.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
 
-
 from invenio.search_engine import perform_request_search
 
 import re
@@ -10,37 +9,83 @@ def printFile(n):
    f = open(n, 'r')
    print f
    
+   search = perform_request_search
+
    p = re.compile('outgoing')
    pA = re.compile("outgoing/[0-9a-zA-Z\.:\-/]*")
    count = 0
    idsFound = 0
    list = []
-   a = {}
+   urls = {}
 
    for line in f:
       line = unquote_plus(line)
       m = p.search(line)
       if m:
-         print 'Match found:', m.group()
-         print line
+         #print 'Match found:', m.group()
          mA = pA.search(line)
          if mA:
-            print "ID:", mA.group(), "\n"
+            #print "ID:", mA.group(), "\n"
             idsFound += 1
             list.append(mA.group())
-            if mA.group() not in a:
-               a[mA.group()] = 1
+            if mA.group() not in urls:
+               urls[mA.group()] = 1
             else:
-               a[mA.group()] = a[mA.group()] + 1
+               urls[mA.group()] = urls[mA.group()] + 1
          count += 1
+
    print "Total outgoing clicks:", count
-   print "IDs found:", idsFound
+   print "URLs found:", idsFound
    #for x in list:
    #   print x, list.count(x)
-   print "Total items:", len(a)
-   c = a.iterkeys()
-   d = a.itervalues()
+   print "Total items:", len(urls)
+
+   urlKeys = urls.iterkeys()
+   urlAppearances = urls.itervalues()
+    
    print "Clicks:", "Paper ID:"
-   for k in a:
-   #  print '{0:7} {1:5}'.format(l, k)
-     print repr(d.next()).rjust(7), c.next()
+   for k in urls:
+     print repr(urlAppearances.next()).rjust(7), urlKeys.next()
+
+   a = re.compile("arx/[a-z-]+/(?P<rid>[0-9]{4}\.[0-9]+)")
+   b = re.compile("arx/(abs|pdf|ps)/(?P<rid>(hep|astro|nucl|gr|quant|cond)-(ph|th|qc|lat|ex|mat)/[0-9]{7})")
+   c = re.compile("doi/[0-9\.]+/(?P<rid>physrevd)\.(?P<rid2>[0-9]*)\.(?P<rid3>[0-9]*)")
+   #regex = {a: "arxiv:" + result.group('rid')}
+   regex = {a: (lambda match: "arxiv:" + match.group('rid')), b: (lambda match: match.group('rid'))}
+   regex[c] = (lambda match: match.group('rid') + " " + match.group('rid2') + " " + match.group('rid3'))
+   rec_ids = {}
+
+   tests = {1: (lambda match: "arxiv:" + match.group('rid')), 2: "hello"}
+
+   for k in urls:
+      hey = False
+      for pattern in regex:
+         result = pattern.search(k)
+         if result:
+            hey = True
+            print result.group()
+            search_results = perform_request_search(p=regex[pattern](result))
+            print search_results
+            if len(search_results) != 1:
+               #regex dictionary needs work
+               print "Make regex for", k
+            else:
+               if search_results[0] not in rec_ids:
+                  rec_ids[search_results[0]] = urls[k]
+               else:
+                  rec_ids[search_results[0]] += urls[k]
+      if hey == False:
+         print result, k
+#         if result:
+#            searchResults = perform_request_search(p=regex[pattern])
+#            print searchResults
+#            if len(searchResults) != 1:
+#               #regex dictionary needs work
+#               print "Make regex for:", k
+#            elif len(searchResults) == 1:
+#               # :)
+#               if searchResults[0] not in recIDs:
+#                  redIDs[searchResults[0]] = urls[k]
+#               else:
+#                  redIDs[searchResults[0]] += urls[k]
+   print rec_ids
