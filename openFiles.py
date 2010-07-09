f75a7fdb5d3dff57b4c37067755e2fcdf80c0554
Ayelet Bitton <bitton@slac.stanford.edu>
2010-07-09 11:45:35 -0700

    Cleaning up: Divided into functions, added command line functionality
    Final changes before re-writing code using a generator, eliminating
    superfluous dictionaries.

--- openFiles.py
+++ openFiles.py
@@ -1,19 +1,23 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
 from invenio.search_engine import perform_request_search
-
+from invenio.bibrank_citation_searcher import get_cited_by_count
+import sys
 import re
 from urllib import unquote_plus
 
-def printFile(n):
-   log_file = open(n, 'r')
-   print log_file
-   
-   search = perform_request_search
+def dissect_log(file_name, rec_ids):
+   """Write something here"""
 
+   file_urls = read_log_file(file_name)
+   return  fill_rec_id_dictionary(rec_ids, file_urls)
+
+def read_log_file(n):
+   """Write something here"""
+   log_file = open(n, 'r')
+   
    url_pattern = re.compile("outgoing/[0-9a-zA-Z\.:\-/]*")
    click_count = 0
-   url_list = []
    urls = {}
 
    for line in log_file:
@@ -22,64 +26,78 @@ def printFile(n):
       if url_match:
          #print 'Match found:', url_match.group(), "\n"
          click_count += 1
-         url_list.append(url_match.group())
          if url_match.group() not in urls:
             urls[url_match.group()] = 1
          else:
             urls[url_match.group()] = urls[url_match.group()] + 1
 
-   print "Total outgoing clicks:", click_count
-   #for x in url_list:
-   #   print x, list.count(x)
-   print "Total distinct clicks:", len(urls)
+   print "Total outgoing clicks:", click_count, "\n"
 
-   urlKeys = urls.iterkeys()
-   urlAppearances = urls.itervalues()
-    
-   print "Clicks:", "Paper ID:"
-   for k in urls:
-     print repr(urlAppearances.next()).rjust(7), urlKeys.next()
+   return urls
 
+def fill_rec_id_dictionary(rec_ids, urls):
+   """Write something here"""
+
+   #regex is a dictionary holding all the possible regular expressions that can be taken from
+   #the urls to search and find the associated rec id of the actual paper. the keys of the regex
+   #dictionary are the possible regular expressions and the values are how to format them to
+   #retrive the appropriate paper when using the function 'perform_request_search'
    a = re.compile("arx/[a-z-]+/(?P<rid>[0-9]{4}\.[0-9]+)")
    regex = {a: (lambda match: "arxiv:" + match.group('rid'))}
    b = re.compile("arx/(abs|pdf|ps)/(?P<rid>(hep|astro|nucl|gr|quant|cond)-(ph|th|qc|lat|ex|mat)/[0-9]{7})")
    regex[b] = (lambda match: match.group('rid'))
-   c = re.compile("doi/[0-9\.]+/(?P<rid>physrevd)\.(?P<rid2>[0-9]*)\.(?P<rid3>[0-9]*)")
-   regex[c] = (lambda match: match.group('rid') + " " + match.group('rid2') + " " + match.group('rid3'))
+   #c = re.compile("doi/[0-9\.]+/(?P<rid>physrevd\.[0-9a-z]*\.[0-9a-z]*)")
+   #regex[c] = (lambda match: (match.group('rid')).replace("." or "/", " " ))
+   c = re.compile("doi/[0-9\.]+/(?P<rid>[a-z\.0-9/-]*)")
+   regex[c] = (lambda match: (((match.group('rid')).replace("/", " ")).replace("-", " ")).replace(".", " "))
 
-   rec_ids = {}
-
-   tests = {1: (lambda match: "arxiv:" + match.group('rid')), 2: "hello"}
+   total_rec_ids = 0
+   total_failed_urls = 0
 
    for k in urls:
       pattern_found = False
       for pattern in regex:
          result = pattern.search(k)
          if result:
-            pattern_found = True
-            print result.group()
             search_results = perform_request_search(p=regex[pattern](result))
-            print search_results
             if len(search_results) != 1:
+               print "Found search term:", regex[pattern](result)
                #regex dictionary needs work
-               print "Make regex for", k
+               if len(search_results) > 100:
+                  print "Search results are very long"
+               elif len(search_results) == 0:
+                  print "Search returned no results for:"
             else:
+               pattern_found = True
                if search_results[0] not in rec_ids:
+                  total_rec_ids += 1
                   rec_ids[search_results[0]] = urls[k]
                else:
                   rec_ids[search_results[0]] += urls[k]
+      #display the url if no pattern in the regex dictionary matched it
       if pattern_found == False:
-         print "No pattern found for:", k, result
-#         if result:
-#            searchResults = perform_request_search(p=regex[pattern])
-#            print searchResults
-#            if len(searchResults) != 1:
-#               #regex dictionary needs work
-#               print "Make regex for:", k
-#            elif len(searchResults) == 1:
-#               # :)
-#               if searchResults[0] not in recIDs:
-#                  redIDs[searchResults[0]] = urls[k]
-#               else:
-#                  redIDs[searchResults[0]] += urls[k]
-   print rec_ids
+         total_failed_urls += 1
+         sys.stderr.write("No Match: " + k + "\n\n")
+   print "Total rec ids:", total_rec_ids
+   print "Total failed URLS:", total_failed_urls
+   
+   return rec_ids
+   
+def print_rec_ids(rec_ids):
+   """Write something here"""
+
+   print "Clicks, Rec ID, Citations:"
+
+   for key in rec_ids:
+      print "%d,%d,%d" % (rec_ids[key], key, get_cited_by_count(key))
+
+def main(args):
+   rec_ids = {}
+
+   for file in args:
+      rec_ids = dissect_log(file, rec_ids)
+
+   print_rec_ids(rec_ids)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
