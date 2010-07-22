8d712ed4a03ec63b78da013075065729c0219956
Ayelet Rachel Bitton <abitton@pcudssw1508.cern.ch>
2010-07-23 00:57:22 +0200

    Added docstrings for create_histogram.py

--- create_histogram.py
+++ create_histogram.py
@@ -3,7 +3,13 @@
 import sys
 import re
 
-def do_stuff(file, overall_dict):
+def read_data(file, overall_dict):
+   """Open each file, and pull the cites and clicks from each line. Then consolidate
+   the data into the new dictionary overall_dict by eliminating the rec id and
+   grouping items by their clicks and citations - creating a new count.
+   """
+
+
    file_contents = open(file, 'r')
 
    clicks_re = re.compile("[0-9]+\s(?P<clicks>[0-9]+)\s(?P<cites>[0-9]+)")
@@ -23,24 +29,32 @@ def do_stuff(file, overall_dict):
 
    return overall_dict
 
+def grid_data(overall_dict):
+   
+
+
+   return overall_dict
+
 def print_data(overall_dict):
+   """Print the data set compiled in read_data."""
 
    test = 0
 
    for clicks in overall_dict:
       for cites in overall_dict[clicks]:
+         #print clicks, cites, frequency
          print "%d %d %d" % (clicks, cites, overall_dict[clicks][cites])
          test += overall_dict[clicks][cites]
 
+   #print total of summed frequency (ie total number of papers)
    print "Test:", test
 
 def main(args):
-   #overall dict: {(clicks, cites): count}
    #overall dict: {clicks: {citations: count}}
    overall_dict = {1: {0: 0}, 2: {0: 0}}
 
    for file in args:
-      overall_dict = do_stuff(file, overall_dict)
+      overall_dict = read_data(file, overall_dict)
 
    print_data(overall_dict)
 
