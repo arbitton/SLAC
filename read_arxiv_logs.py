f13a5857871a009119b0a4fd28a1b02f6c7a3367
Ayelet Rachel Bitton <abitton@pcudssw1508.cern.ch>
2010-08-02 18:12:44 +0200

    Creating program to read arXiv logs

--- read_arxiv_logs.py
+++ read_arxiv_logs.py
@@ -16,20 +16,15 @@ from invenio.search_engine import get_fieldvalues
 arxiv_general_pattern = re.compile("arx/(mirr/)*[a-z-]+/(?P<rid>[0-9]{4}\.[0-9]+)")
 arxiv_secondary_general_pattern = re.compile("arx/(mirr/)*((abs|pdf|ps)/)*(?P<rid>[a-z/0-9-]+)")
 arxiv_specific_pattern = re.compile("arx/(mirr/)*((abs|pdf|ps)/)*(?P<rid>(hep|astro|nucl|gr|quant|cond)-(ph|th|qc|lat|ex|mat)/[0-9]{7})")
-doi_pattern = re.compile("doi/(http://dx.doi.org/)*[0-9\.]+/(?P<rid>[a-z\.0-9/)(-]*)")
-physrevlett_pattern = re.compile("doi/(http://dx.doi.org/)*(?P<rid>[0-9.]{7}/(physrevd|j.physrep|physrev|aph|ptps|epjc/s|j.nuclphysb|j.physletb|physics|physrevlett|j.astropartphys|j.nuclphysbps).[0-9./-]+)")
-doi_general_pattern = re.compile("doi/(http://dx.doi.org/)*(?P<rid>[0-9)(/.-]+)")
 regex = {arxiv_specific_pattern: (lambda match: '037:' + match.group('rid')),
          arxiv_secondary_general_pattern: (lambda match: '037:' + match.group('rid')),
          arxiv_general_pattern: (lambda match: '"arxiv:' + match.group('rid') + '"'), 
-         doi_pattern: (lambda match: (((((match.group('rid')).replace("/", " ")).replace("-", " ")).replace(".", " ")).replace(")", " ")).replace("(", " ")),
-         physrevlett_pattern: (lambda match: '773:' + match.group('rid')), 
-         doi_general_pattern: (lambda match: match.group('rid')),
         }
 
-blacklisted_ips = ["130.199.3.130", "128.250.54.23", "189.50.0.3", "156.35.192.2", "84.237.121.107", "140.105.47.82", "143.233.250.165", "147.46.56.157", "129.186.151.231", "210.117.131.100", "131.220.8.9", "221.116.19.146", "129.13.72.198", "130.199.3.140", "195.251.115.254", "122.130.177.184", "134.107.3.147", "193.206.153.230", "200.145.46.252", "149.156.47.236", "131.111.16.20", "192.16.204.74", "129.104.3.5", "202.41.93.129", "218.130.209.108", "194.80.32.11", "210.210.15.150", "129.70.124.44", "162.105.246.189", "192.16.204.77", "132.77.4.129", "132.77.4.43", "141.2.247.4", "147.156.163.88", "140.105.47.84", "194.94.224.254", "155.230.152.39", "129.234.4.76", "130.54.55.3", "149.217.1.6", "129.2.175.71", "130.225.212.4", "163.239.43.45", "210.219.50.14", "220.227.103.131", "129.234.4.10", "155.230.153.238", "159.93.14.8", "38.117.109.20", "202.160.174.4", "202.122.32.131"]
+# blacklisted_ips = ["130.199.3.130", "128.250.54.23", "189.50.0.3", "156.35.192.2", "84.237.121.107", "140.105.47.82", "143.233.250.165", "147.46.56.157", "129.186.151.231", "210.117.131.100", "131.220.8.9", "221.116.19.146", "129.13.72.198", "130.199.3.140", "195.251.115.254", "122.130.177.184", "134.107.3.147", "193.206.153.230", "200.145.46.252", "149.156.47.236", "131.111.16.20", "192.16.204.74", "129.104.3.5", "202.41.93.129", "218.130.209.108", "194.80.32.11", "210.210.15.150", "129.70.124.44", "162.105.246.189", "192.16.204.77", "132.77.4.129", "132.77.4.43", "141.2.247.4", "147.156.163.88", "140.105.47.84", "194.94.224.254", "155.230.152.39", "129.234.4.76", "130.54.55.3", "149.217.1.6", "129.2.175.71", "130.225.212.4", "163.239.43.45", "210.219.50.14", "220.227.103.131", "129.234.4.10", "155.230.153.238", "159.93.14.8", "38.117.109.20", "202.160.174.4", "202.122.32.131"]
 
 def dissect_log(n, rec_ids):
+
    for line in log_url_filter(n):
       rec_ids = url_count(line, rec_ids)
    
@@ -38,8 +33,6 @@ def dissect_log(n, rec_ids):
 def log_url_filter(n):
    log_file = open(n, 'r')
    
-   url_pattern = re.compile("Pid\s[0-9]+\s(?P<ip_add>[0-9.]+)\s.*outgoing/[0-9a-zA-Z\.:\-/]*")
-
    for line in log_file:
       line = unquote_plus(line)
       url_match = url_pattern.search(line)
